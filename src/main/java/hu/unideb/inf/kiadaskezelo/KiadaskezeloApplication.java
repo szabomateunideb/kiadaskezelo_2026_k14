dd6cda04c1a3c6201d5ab25d53e5606f566ccd39
Máté <szabo.mate@inf.unideb.hu>
2026-09-15 15:16:58 +0200

    Init

---
+++ src/main/java/hu/unideb/inf/kiadaskezelo/KiadaskezeloApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.kiadaskezelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiadaskezeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiadaskezeloApplication.class, args);
+	}
+
+}
